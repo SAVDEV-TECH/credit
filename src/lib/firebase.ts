2a4ba7ba2d4fdfdcbcddfae7da380863095fbaa8
SavdevTech <savde388@gmail.com>
2026-05-18 07:59:56 -0700

    fix: safeguard firebase initialization during build time

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,9 +5,9 @@ import { getStorage } from "firebase/storage";
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env['NEXT_PUBLIC_FIREBASE_API_KEY'],
+  apiKey: process.env['NEXT_PUBLIC_FIREBASE_API_KEY'] || "dummy-api-key-for-build",
   authDomain: process.env['NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN'],
-  projectId: process.env['NEXT_PUBLIC_FIREBASE_PROJECT_ID'],
+  projectId: process.env['NEXT_PUBLIC_FIREBASE_PROJECT_ID'] || "dummy-project-id",
   storageBucket: process.env['NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET'],
   messagingSenderId: process.env['NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID'],
   appId: process.env['NEXT_PUBLIC_FIREBASE_APP_ID'],
